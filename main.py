03fbf7f0cb4b4008dddaa093402ad226b90dd896
JakubRobo <131374444+JakubRobo@users.noreply.github.com>
2023-04-29 20:53:27 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+imie = input('Podaj swoje imię: ')
+wiek = input('Podaj swój wiek: ')
+
+wiek = int(wiek)
+if(wiek > 32):
+    print('Mów mi python, mam 32 lata. Witaj w moim świecie '+ imie + '. Jesteś starszy ode mnie.')
+
+else:
+    print('Mów mi python, mam 32 lata. Witaj w moim świecie '+ imie + '. Jesteś młodszy ode mnie.')
+
+=======
+print("Hello")
+>>>>>>> b537f21b8b23778361690f2f6e1a8eca1be7b372
